c979bf4adb3782b7fe6ef99f94999db1f5706abb
rionshocker <114156943+rionshocker@users.noreply.github.com>
2022-10-23 02:07:52 +0800

    Create path_elements_of_programming.js

---
+++ paths/path_elements_of_programming.js
@@ -0,0 +1,30 @@
+//Question 1
+//2 + 3
+
+//Question 2
+//25;
+
+//Question 3
+const foo = 10;
+const bar = 10 + 20;
+// your solution goes here
+const foobar = foo + bar;
+foobar;
+
+//Question 4
+// your solution goes here
+function bar() {
+    return 20;
+}
+bar();
+
+//Question 5
+function square(x) {
+    return x * x;
+}
+
+// your solution goes here
+function square_of_difference(x, y) {
+    return square(x - y);
+}
+square_of_difference(2, 5);

c6724fb6803e235042d29cb7c39681248724adf9
rionshocker <114156943+rionshocker@users.noreply.github.com>
2022-10-23 02:12:42 +0800

    Create path_substitution_model_and_recursion.js

---
+++ paths/path_substitution_model_and_recursion.js
@@ -0,0 +1,26 @@
+//TASK 1
+//Normal order reduction
+
+//TASK 2
+//Applicative order reduction
+
+//TASK 3
+//Applicative order reduction
+
+//TASK 4
+//Iterative process
+
+//TASK 5
+//Recursive process
+
+//TASK 6
+//Steps 2
+
+//TASK 7
+//Steps 1
+
+//TASK 8
+//The program gives rise to a recursive process because deferred operations accumulate.
+
+//TASK 9
+//The program gives rise to an iterative process because no deferred operations accumulate.

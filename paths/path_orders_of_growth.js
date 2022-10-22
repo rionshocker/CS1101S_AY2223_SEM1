50f12f8af1ba1c6d0eca898b8a19a42f8d25129f
rionshocker <114156943+rionshocker@users.noreply.github.com>
2022-10-23 02:15:03 +0800

    Create path_orders_of_growth.js

---
+++ paths/path_orders_of_growth.js
@@ -0,0 +1,23 @@
+//QUESTION 1
+//The function t has order of growth θ(n).
+
+//QUESTION 2
+//The function s has order of growth θ(n).
+
+//QUESTION 3
+//The function t has order of growth θ(n).
+
+//QUESTION 4
+//θ(1)
+
+//QUESTION 5
+//O(2^n)
+
+//QUESTION 6
+//a * n
+
+//QUESTION 7
+//θ(n)
+
+//QUESTION 8
+//θ(log(n))

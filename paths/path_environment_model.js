1cba6e1c4cc04fa5bafdb440ac924d1e1124e911
rionshocker <114156943+rionshocker@users.noreply.github.com>
2022-10-23 02:35:04 +0800

    Create path_environment_model.js

---
+++ paths/path_environment_model.js
@@ -0,0 +1,44 @@
+//Q1
+//An environment is a series of frames.
+
+//Q2
+//A frame is a set of name bindings.
+
+//Q3
+//The interpreter looks up the variable in the frame that is enclosing the current frame.
+
+//Q4
+//The interpreter issues an error message "Name not declared".
+
+//Q5
+//The interpreter uses the same environment in which the function application is evaluated.
+
+//Q6
+//The interpreter uses a new environment, which results from extending the environment stored in the function object that the name f refers to.
+
+//Q7
+//Only the parameters of the function being called.
+
+//Q8
+//In the current frame if it has a binding of x or the closest surrounding frame that has a binding of x.
+
+//Q9
+//The environment in which the statement gets evaluated.
+
+//Q10
+//2
+
+//Q11
+//10
+
+//Q12
+//9
+
+//Q13
+//4
+
+//Q14
+//10
+
+//Q15
+//3

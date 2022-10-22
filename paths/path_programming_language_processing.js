a7ab8d860148c1872e65e2a396850d16b6b2ae72
rionshocker <114156943+rionshocker@users.noreply.github.com>
2022-10-23 02:23:55 +0800

    Create path_programming_language_processing.js

---
+++ paths/path_programming_language_processing.js
@@ -0,0 +1,17 @@
+//Question 1
+//This diagram depicts an interpreter that can interpret programs written in Swift.
+
+//Question 2
+//This diagram depicts a compiler that produces programs written in C.
+
+//Question 3
+//The diagram is correct.
+
+//Question 4
+//The diagram is correct.
+
+//Question 5
+//The diagram is incorrect because the to-language doesn't match.
+
+//Question 6
+//The diagram is correct.
